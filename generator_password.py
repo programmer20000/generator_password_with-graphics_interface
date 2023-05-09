7e238284241c996706aa7450e4f3143f72b7b29b
Mihai Apostol <mihaiapostol153@gmail.com>
2023-05-09 15:22:36 +0300

    update all interface

--- generator_password.py
+++ generator_password.py
@@ -1,69 +1,56 @@
-from tkinter import *
-import pyperclip
 from random import randint
-from tkinter.messagebox import showinfo
+from tkinter.messagebox import showwarning
+
+import customtkinter
+import pyperclip
+
 from symbols_for_password import symbols_for_password
 
 
-class Window(Frame):
-    def __init__(self, window):
-        super().__init__(window)
+class MyFrame(customtkinter.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
 
-        self.password = ''
+        self.length_password_input = customtkinter.CTkEntry(self, placeholder_text='Please Enter length password')
+        self.length_password_input.grid(row=0, column=0, padx=100, pady=10)
 
-        self.show_password = StringVar()
+        self.show_password = customtkinter.CTkLabel(self, text='yours password ', fg_color="transparent")
+        self.show_password.grid(rowspan=2, columnspan=2, padx=100, pady=10)
 
-
-        self.label_information = Label(window, text='Enter count symbol for password', font=('Arial', 10))
-        self.label_information.pack(pady=6)
-
-        self.count_symbol_for_password_filed = Entry(window, width=20)
-        self.label_copy_password = Entry(window, width=20,state='readonly',textvariable=self.show_password)
-
-        self.count_symbol_for_password_filed.pack(pady=10)
-        self.label_copy_password.pack(pady=12)
-
-        self.label_information = Button(window, text='generate and password', command=self.generator_password,
-                                        font=('Arial', 10))
-
-        self.label_information.pack(pady=6)
+        self.button = customtkinter.CTkButton(self, text="CTkButton", command=self.generator_password)
+        self.button.grid(rowspan=2, columnspan=2, padx=100, pady=20)
 
     def generator_password(self):
         try:
-            self.get_value_from_input_box = int(self.count_symbol_for_password_filed.get())
-
-
+            self.question_user = int(self.length_password_input.get())
 
             self.password = ''
 
-            if self.get_value_from_input_box > len(symbols_for_password):
-                showinfo('Message', 'Please enter count symbol for password only 1 to 2048')
+            if self.question_user > len(symbols_for_password):
+                return showwarning('Warning', 'Please enter count symbol for password only 1 to 2048')
 
-                '''
-                generating password with algorithm below
-                copy password with help library pyperclip 
-                '''
-
-            for i in range(self.get_value_from_input_box):
+            for i in range(self.question_user):
                 self.password = f'{self.password}{symbols_for_password[randint(0, len(symbols_for_password) - 1)]}'
-            self.show_password.set(self.password)
+            self.show_password.configure(text=self.password)
             pyperclip.copy(self.password)
-            showinfo('Message', 'Successful copycatted password')
+            return showwarning('Message', 'Yours password successful copy')
 
-        except Exception as exception:
-            showinfo('Message', 'Please Enter only number')
+        except ValueError:
+            return showwarning('Warning', 'Please enter only number values')
 
 
-class App(Tk):
+
+class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-
-        self.title('generator password'.upper())
-        self.geometry('250x200')
-        self.resizable(False, False)
+        self.geometry("400x200")
+        self.title('Generator Password')
+        self.grid_rowconfigure(0, weight=1)  # configure grid system
+        self.grid_columnconfigure(0, weight=1)
+        self.my_frame = MyFrame(master=self)
+        self.my_frame.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
 
 
 if __name__ == '__main__':
     app = App()
-    window = Window(app)
     app.mainloop()
